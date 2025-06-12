59df12e7856b57a974491b8de8fac1af6a1c6d61
PridymaiXD <xdpridymai@gmail.com>
2025-06-12 14:36:18 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,9 @@
 const inputElement = document.querySelector("input#name-input");
 const outputElement = document.querySelector("input#name-output")
 inputElement.addEventListener("input", (event) => {
-    console.log(event);
-    console.log(event.currentTarget);
-    console.log(event.currentTarget.value.trim());
     if(inputElement.value.trim()){
 outptElement.innerHTML = inputElement.value.trim()
 } else {
 outputElement.innerHTML = "Anonymous"
 }
   });
-
- `<input type="text" id="name-input" placeholder="Please enter your name" />
-<h1>Hello, <span id="name-output">Anonymous</span>!</h1>`
