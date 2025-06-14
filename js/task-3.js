7853f045aef77cb89468435083bf573e48145e3f
PridymaiXD <xdpridymai@gmail.com>
2025-06-14 11:05:55 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const inputElement = document.querySelector("input#name-input");
 const outputElement = document.querySelector("span#name-output")
 inputElement.addEventListener("input", (event) => {
     if(event.currentTarget.value.trim()){
-outptElement.innerHTML = event.currentTarget.value.trim()
+outputElement.innerHTML = event.currentTarget.value.trim()
 } else {
 outputElement.innerHTML = "Anonymous"
 }
